6875a84bc7ebac48be068d1be97035a5f9d2b5ef
NZAFIZAR <nathan.zafizara@gmail.com>
2023-10-18 10:35:34 +0200

    Adding basic classes

---
+++ src/main/java/fr/nathzaf/projects/mavenproject/HelloWorld.java
@@ -0,0 +1,7 @@
+package fr.nathzaf.projects.mavenproject;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/fr/nathzaf/projects/mavenproject/SimpleTest.java
@@ -0,0 +1,13 @@
+package fr.nathzaf.projects.mavenproject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void simpleTest(){
+        assertEquals(0, 0);
+    }
+}
